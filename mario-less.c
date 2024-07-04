652c494dc46720b10dcfc18835a775ce7bd4dc8b
iLearnCS50 <holeesheyit@tutamail.com>
2024-07-04 11:55:34 +0530

    Create mario-less.c

---
+++ mario-less.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    int j = 0;
+    do
+    {
+        n = get_int("Height:");
+    }
+    while (n < 1 || n > 8);
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = j; k < n - 1; k++)
+        {
+            printf(" ");
+        }
+        for (j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
